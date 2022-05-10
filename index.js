6c3330dde1af9faa06c81cc60f9ae0aabea159ea
jenishkanani <jenish@appzera.co.in>
2022-05-10 18:27:06 +0530

    base version of node mailer mail send app

---
+++ index.js
@@ -0,0 +1,24 @@
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jenishkanani00@gmail.com',
+        pass: 'jenishkananiR7359$'
+    }
+});
+
+var mailOptions = {
+    from: 'jenishkanani00@gmail.com',
+    to: 'jenishkanani73@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
